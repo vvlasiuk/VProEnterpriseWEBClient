b758b178b97a445215587f6e14808396bf82b6ca
Volodymyr <vlsv00@gmail.com>
2025-11-23 12:10:51 +0200

    ServerOffline

---
+++ src/pages/ServerOffline.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ServerOffline = ({ onRetry }) => (
+  <div style={{ textAlign: 'center', marginTop: '50px' }}>
+    <h2>Сервер недоступний</h2>
+    <p>Будь ласка, перевірте підключення або спробуйте ще раз.</p>
+    <button onClick={onRetry}>Спробувати ще раз</button>
+  </div>
+);
+
+export default ServerOffline;
